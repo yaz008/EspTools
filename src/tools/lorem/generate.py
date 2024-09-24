1767ec3b23b379e38e855b916fc66cd88fdaf15f
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-09-24 20:45:50 +0300

    Add tools.lorem.generate

---
+++ src/tools/lorem/generate.py
@@ -0,0 +1,19 @@
+from random import choices
+
+def generate_sentence(length: int, words: list[str]) -> None:
+    k: int = max(0, length - 2)
+    words: list[str] = ['Lorem', 'ipsum'] + choices(words, k=k)
+    return ' '.join(words[:length])
+    
+def generate_lorem(tokens: list[str], words: list[str]) -> str:
+    lorem: str = ''
+    for token in tokens:
+        match token:
+            case '.':
+                lorem += '. '
+            case '/':
+                lorem += '\n'
+            case number:
+                lorem += generate_sentence(length=int(number),
+                                           words=words)
+    return lorem
--- src/tools/lorem/main.py
+++ src/tools/lorem/main.py
@@ -1,27 +1,9 @@
 from os import getenv
 from sys import path
-from random import choices
 from core.filemanip import load_json
 from core.tokenizer import Tokenizer
 from tools.lorem.grammar import LoremGrammar
-
-def generate_sentence(length: int, words: list[str]) -> None:
-    k: int = max(0, length - 2)
-    words: list[str] = ['Lorem', 'ipsum'] + choices(words, k=k)
-    return ' '.join(words[:length])
-    
-def generate_lorem(tokens: list[str], words: list[str]) -> str:
-    lorem: str = ''
-    for token in tokens:
-        match token:
-            case '.':
-                lorem += '. '
-            case '/':
-                lorem += '\n'
-            case number:
-                lorem += generate_sentence(length=int(number),
-                                           words=words)
-    return lorem
+from tools.lorem.generate import generate_sentence, generate_lorem
 
 def main() -> str:
     expr: str = getenv(key='ESPANSO_LOREM')
