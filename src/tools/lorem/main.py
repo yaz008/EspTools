05ef0ff362177ca31c488c01264b382b153a34f6
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-09-24 15:59:04 +0300

    Add tools.lorem

---
+++ src/tools/lorem/main.py
@@ -0,0 +1,31 @@
+from os import getenv
+from sys import path
+from random import choices
+from utils.filemanip import load_json
+from core.tokenizer import Tokenizer
+
+def generate_sentence(length: int, words: list[str]) -> None:
+    if length in (0, 1, 2):
+        return ' '.join(['Lorem', 'ipsum'][:length])
+    else:
+        return ' '.join(['Lorem', 'ipsum'] + choices(words, k=length - 2))
+    
+def generate_lorem(tokens: list[str], words: list[str]) -> str:
+    lorem: str = ''
+    for token in tokens:
+        match token:
+            case '.':
+                lorem += '. '
+            case '/':
+                lorem += '\n'
+            case number:
+                lorem += generate_sentence(length=int(number),
+                                           words=words)
+    return lorem
+
+def main() -> str:
+    expr: str = getenv(key='ESPANSO_LOREM')
+    words: list[str] = load_json(path=f'{path[0]}\\tools\\lorem\\words.json')
+    tokenizer: Tokenizer = Tokenizer(grammar=r'[\d]+|[\.]|[/]')
+    tokens: list[str] = tokenizer.tokenize(expr=expr)
+    return generate_lorem(tokens=tokens, words=words)
